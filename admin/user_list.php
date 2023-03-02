f9b2f9ee2c332c1ca511a4a9b6e7a721b841d627
Jamer Catalla <mr.catalla28@gmail.com>
2023-03-02 13:33:40 +0800

    Update User list icon

--- admin/user_list.php
+++ admin/user_list.php
@@ -16,11 +16,9 @@ require '../components/navigation.php';
         <?php if($role == 'councilor'){ ?>
         <button class="btn btn-primary" 
         data-bs-toggle="modal" data-bs-target="#createNewAccount">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-add" viewBox="0 0 16 16">
-            <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
-            <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
-            </svg>
-        Create Account</button>
+        <i class="bi bi-person-plus"></i>
+        Create Account
+        </button>
         <?php } ?>
         </div>
         <div class="table-responsive my-3">
@@ -48,21 +46,21 @@ require '../components/navigation.php';
                         <td>
                         <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" 
                         data-bs-target="#viewAccount_<?= $users->user_id; ?>">
-                        <i class="bi bi-search"></i>
+                        <i class="bi bi-search">View</i>
                         </button>
                         </td>
                         <?php if($role == 'councilor'){ ?>
                         <td>
                         <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" 
                         data-bs-target="#updateAccount_<?= $users->user_id; ?>">
-                        <i class="bi bi-pencil-square"></i>
+                        <i class="bi bi-pencil-square">Update</i>
                         </button>
                        
                         </td>
                         <td>
                         <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" 
                         data-bs-target="#deactivateAccount">
-                        Active
+                        <i class="bi bi-person-check">Active</i>
                         </button>
                         </td>
                         <?php } ?>
