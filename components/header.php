72eda294bc4dea891d9625ae7a6c3f0328d8a4f7
Jamer Catalla <mr.catalla28@gmail.com>
2023-03-02 22:45:11 +0800

    Change bg color to light

--- components/header.php
+++ components/header.php
@@ -11,4 +11,4 @@
     <!-- Bootstrap Icon -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   </head>
-  <body>
+  <body class="bg-light">
