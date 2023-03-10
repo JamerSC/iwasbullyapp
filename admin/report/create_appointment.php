e9194c3c304a21d25efabc531a326f4510885238
Jamer Catalla <mr.catalla28@gmail.com>
2023-03-10 09:53:07 +0800

    Update Variable display

--- admin/report/create_appointment.php
+++ admin/report/create_appointment.php
@@ -7,7 +7,21 @@ require_once 'connection.php';
 // Check if the form is submitted
 if (isset($_POST['appointment'])){
 
+    $ReportID = $_GET['id'];
 
+    $UserID = $_SESSION['UserID'];
+
+    $AppointmentDate = $_POST['$AppointmentDate'];
+    $AppointmentDay = $_POST['$AppointmentDay'];
+    $StartTime = $_POST['StartTime'];
+    $EndTime = $_POST['EndTime'];
+
+    $CreatedBy = $_SESSION['UserID'];
+
+    //$stmt = $conn->prepare("INSERT INTO Appointment (AppointmentDate, AppointmentDay
+    //StartTime, EndTime, CreatedBy)");
+
+    echo $ReportID;
 
 }
 ?>
