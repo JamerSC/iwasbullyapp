703a8cf9bcd16d3eb703b00c882153ca9f2d4ad5
Jamer Catalla <mr.catalla28@gmail.com>
2023-03-10 11:52:57 +0800

    Insert Appointment

--- admin/report/create_appointment.php
+++ admin/report/create_appointment.php
@@ -11,17 +11,48 @@ if (isset($_POST['appointment'])){
 
     $UserID = $_SESSION['UserID'];
 
-    $AppointmentDate = $_POST['$AppointmentDate'];
-    $AppointmentDay = $_POST['$AppointmentDay'];
+    $AppointmentDate = $_POST['AppointmentDate'];
+    $AppointmentDay = $_POST['AppointmentDay'];
     $StartTime = $_POST['StartTime'];
     $EndTime = $_POST['EndTime'];
 
     $CreatedBy = $_SESSION['UserID'];
 
-    //$stmt = $conn->prepare("INSERT INTO Appointment (AppointmentDate, AppointmentDay
-    //StartTime, EndTime, CreatedBy)");
+    try {
 
-    echo $ReportID;
+        if(!empty($AppointmentDay))
+        {
 
+            $stmt = $conn->prepare("INSERT INTO Appointment 
+            (UserID, ReportID, AppointmentDate, AppointmentDay, 
+            StartTime, EndTime, CreatedBy)
+            VALUES (:UserID, :ReportID, :AppointmentDate, :AppointmentDay, 
+            :StartTime, :EndTime, :CreatedBy)");
+            $stmt->execute([
+                'UserID' => $UserID,
+                'ReportID' => $ReportID,
+                'AppointmentDate' => $AppointmentDate,
+                'AppointmentDay' => $AppointmentDay, 
+                'StartTime' => $StartTime,
+                'EndTime' => $EndTime,
+                'CreatedBy' => $CreatedBy
+            ]);
+
+            echo " <script>alert('Created Report Succesfuly!!')</script>";
+            echo "<script>window.location = '../report.php'</script>";
+        }
+        else
+        {
+            echo " <script>alert('Please Select Day!!')</script>";
+            echo "<script>window.location = '../report.php'</script>";
+        }
+
+    } 
+    catch (PDOException $e) 
+    {
+        echo "Connection Failed " . $e->getMessage();
+        die();
+    
+    }
 }
 ?>
