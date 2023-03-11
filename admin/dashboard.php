288bc3ee4cd35a1efa46010294fbdfb112a8d587
Jamer Catalla <mr.catalla28@gmail.com>
2023-03-11 14:18:58 +0800

    Insert Uploads folder, updte post, dash, align

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,53 +3,24 @@ require '../function/session.php';
 require '../components/header.php'; 
 require '../components/navigation.php';
 ?>
-    <div class="container">
-        <h3>Dashboard</h3>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">Users</div>
-                    <div class="card-body">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Juan</td>
-                                    <td>juan@gmail.com</td>
-                                    <td>1</td>
-                                    <td>Juan</td>
-                                    <td>juan@gmail.com</td>
-                                    
-                                </tr>
-                                <!-- 
-                                
-                                // Retrieve users from database and display in table
-                                $stmt = $pdo->query("SELECT * FROM users");
-                                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                                    echo "<tr>";
-                                    echo "<td>" . $row['id'] . "</td>";
-                                    echo "<td>" . $row['name'] . "</td>";
-                                    echo "<td>" . $row['email'] . "</td>";
-                                    echo "</tr>";
-                                }
-                            -->
-                                
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="container-fluid">
+    <h3 class="fs-1">Dashboard</h3>
+    <div class="row">
+        <div class="col-sm-6">
+        <h2>Panel 1</h2>
+        <p>Some text goes here.</p>
     </div>
+    <div class="col-sm-6">
+        <h2>Panel 2</h2>
+        <p>Some text goes here.</p>
+    </div>
+    </div>
+     <div class="row">
+    <div class="col-sm-12">
+    <h2>Chart</h2>
+        <canvas id="myChart"></canvas>
+    </div>
+    </div>
+</div>
 
 <?php require '../components/footer.php' ;?>
--- admin/post.php
+++ admin/post.php
@@ -1,66 +1,64 @@
 <?php
 
 require '../function/session.php';
+require_once '../connection/DBconnection.php';
 require '../components/header.php'; 
 require '../components/navigation.php';
 ?>
 
-  <h3>Post</h3>
-
-
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-  Create Post
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Create New Post</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <!-- Post form -->
-
-        <form action="create_post.php" method="post" enctype="multipart/form-data">
-          <div class="form-group mb-3">
-            <label class="input-group-text" for="title">Title</label>
-            <input type="text" class="form-control" id="title" name="title" required>
-          </div>
-          <div class="form-group mb-3">
-            <label class="input-group-text" for="content">Content</label>
-            <textarea class="form-control" id="content" name="content" required></textarea>
-          </div>
-          <div class="form-group mb-3">
-            <label class="input-group-text" for="image">Image</label>
-            <input type="file" class="form-control-file" id="image" name="image" required>
-          </div>
-          <button type="submit" class="btn btn-primary form-control">Post</button>
-        </form>
-
-        
-        <!--
-        <form method="post" action="add_post.php">
-          <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" class="form-control" id="title" name="title">
-          </div>
-          <div class="form-group">
-            <label for="content">Content</label>
-            <textarea class="form-control" id="content" name="content" rows="5"></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary">Add Post</button>
-        </form> 
-          -->
-        
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary form-control" data-bs-dismiss="modal">Cancel</button>
-      </div>
+<div class="position-relative my-3">
+  <h1 class="fs-1">HOME</h1>
+  <div class="position-absolute top-10 start-50 translate-middle-x grid gap-3">
+    <div class="p-4 m-4 g-col-6 bg-info">
+      <form action="create_post.php" method="POST" enctype="multipart/form-data">
+        <div class="form-group">
+          <label for="title">Title</label>
+          <input type="text" class="form-control" id="title" name="title">
+        </div>
+        <br>
+        <div class="form-group">
+          <label for="content">Content</label>
+          <textarea class="form-control" id="content" name="content"></textarea>
+        </div>
+        <br>
+        <div class="form-group">
+          <label for="image">Image</label>
+          <input type="file" class="form-control-file" id="image" name="image">
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Create Post</button>
+      </form>
+      <br><br>
     </div>
+
+<?php 
+
+    //$stmt = $conn->prepare("SELECT image FROM images");
+    $stmt = $conn->prepare("SELECT image, post FROM images ORDER BY img_id DESC"); 
+    //Descending order using id or date/created at
+
+    $stmt->execute();
+    $images = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($images as $image) {
+        $url = 'uploads/' . $image['image'];
+        $posted = $image['post'];
+
+        echo '<div class="p-4 m-5 g-col-6 bg-info">';
+        echo '<a target="_blank" href="' . $url . '">';   //Open new tab for new image
+        echo '<img src="' . $url . '" alt="report" style="width: 100%; display: flex; height: auto;">'; //display image
+        echo '</a>';
+        echo '<br>';
+        echo '<br>';
+        echo '<p>'. $posted .'</p>';
+        echo '<br>';
+        echo '</div>';
+    }
+?>
   </div>
-</div>
+</div> 
+
+
+
 
 <?php require '../components/footer.php' ;?>
